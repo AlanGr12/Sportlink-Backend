aecc5565fd996e5d21d7d016dd21e3174c6a262c
JUAN SEBASTIAN CARUS <48802882@ort>
2026-08-07 09:05:54 -0300

    avz

--- src/entities/club.js
+++ src/entities/club.js
@@ -1,11 +1,12 @@
 class Club {
-  constructor({ idclub, idusuario, nombre, ubicacion, fotoperfil, descripcion } = {}) {
+  constructor({ idclub, idusuario, nombre, ubicacion, fotoperfil, descripcion, deportes } = {}) {
     this.idclub = idclub
     this.idusuario = idusuario
     this.nombre = nombre
     this.ubicacion = ubicacion
     this.fotoperfil = fotoperfil
     this.descripcion = descripcion
+    this.deportes = deportes || []
   }
 }
 
